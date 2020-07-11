803875ca461e86e4540a2b2bf69c429fd00afdf2
Witek85 <witold.szklarski@gmail.com>
2020-07-11 17:34:12 +0200

    delete todo

--- src/app/services/todos.service.ts
+++ src/app/services/todos.service.ts
@@ -1,21 +1,28 @@
 import { Injectable } from '@angular/core';
-// import { Subject } from 'rxjs';
+import { Subject } from 'rxjs';
 import { TodoList } from '../todolist.model';
+
+const todosy = [
+  {id: 1, task: 'Do something', priority: 'Normal'},
+  {id: 2, task: 'Do something else', priority: 'Normal'},
+  {id: 3, task: 'Do something completely different!', priority: 'Urgent'},
+]
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodosService {
-  // todoSubject = new Subject<TodoList[]>();
-  todolist: TodoList[] = [
-    {id: 1, task: 'Do something', priority: 'Normal'},
-    {id: 2, task: 'Do something else', priority: 'Normal'},
-    {id: 3, task: 'Do something completely different!', priority: 'Urgent'},
-  ];
+  todoChange = new Subject<TodoList[]>();
+  todolist: TodoList[] = todosy;
 
   constructor() { }
 
   getTodos() {
     return this.todolist.slice();
+  }
+  
+  deleteTodo(id) {
+    this.todolist = this.todolist.filter(todo => todo.id !== id);
+		this.todoChange.next(this.todolist.slice());
 	}
 }
--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { TodosService } from '../services/todos.service';
 import { TodoList } from '../todolist.model';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-todo-list',
@@ -10,11 +11,25 @@ import { TodoList } from '../todolist.model';
 export class TodoListComponent implements OnInit {
   displayedColumns: string[] = ['id', 'task', 'priority', 'actions'];
   todoList: TodoList[];
+  todoSubscription: Subscription;
 
   constructor(private todosService: TodosService) { }
 
   ngOnInit() {
+    this.todoSubscription = this.todosService.todoChange.subscribe(
+      (todoList: TodoList[]) => {
+        this.todoList = todoList;
+      }
+    );
     this.todoList = this.todosService.getTodos();
+  }
+
+  ngOnDestroy() {
+    this.todoSubscription.unsubscribe();
+  }
+
+  onDelete(id) {
+    this.todosService.deleteTodo(id);
   }
 
 }
