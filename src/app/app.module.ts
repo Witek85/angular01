972e2e2237ba4daa6bf4bdb2bc50c40187ee99de
Witek85 <witold.szklarski@gmail.com>
2020-07-18 23:53:43 +0200

    moving routing to module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppMaterialModule } from './app-material.module';
 import { AppComponent } from './app.component';
 import { SecondComponent } from './second/second.component';
-
-
-const routes = [
-  { path: '', redirectTo: '', pathMatch: 'full' },
-  { path: 'first', loadChildren: './first/first.module#FirstModule'},
-  { path: 'second', component: SecondComponent },
-  { path: 'todos',  loadChildren: './todo/todo.module#TodoModule'},
-  { path: 'machines',  loadChildren: './machines/machines.module#MachinesModule'},
-  { path: '**', redirectTo: '/' }
-];
+import { AppRoutingModule } from './app.routing.module';
 
 @NgModule({
   declarations: [
@@ -28,7 +18,7 @@ const routes = [
     BrowserModule,
     BrowserAnimationsModule,
     AppMaterialModule,
-    RouterModule.forRoot(routes),
+    AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule
---
+++ src/app/app.routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { SecondComponent } from './second/second.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '', pathMatch: 'full' },
+  { path: 'first', loadChildren: './first/first.module#FirstModule'},
+  { path: 'second', component: SecondComponent },
+  { path: 'todos',  loadChildren: './todo/todo.module#TodoModule'},
+  { path: 'machines',  loadChildren: './machines/machines.module#MachinesModule'},
+  { path: '**', redirectTo: '/' }
+];
+
+@NgModule({
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule]
+})
+export class AppRoutingModule {}
