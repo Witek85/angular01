b15b2ab418f115160a359b078117784ffe89b335
Witek85 <witold.szklarski@gmail.com>
2020-07-28 00:40:44 +0200

    hostlistener hostbinding

--- src/app/counter/directives/highlight.directive.ts
+++ src/app/counter/directives/highlight.directive.ts
@@ -1,15 +1,28 @@
-import { Directive, ElementRef, OnInit } from '@angular/core';
+import { Directive, ElementRef, OnInit, Renderer2, HostListener, HostBinding } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective implements OnInit {
+  @HostBinding('style.backgroundColor') background: string = 'white';
 
-  constructor(private elementRef:ElementRef) {}
+  constructor(private elementRef:ElementRef, private renderer:Renderer2) {}
 
   ngOnInit() {
     console.log('dyrektywa', this.elementRef)
-    this.elementRef.nativeElement.style.backgroundColor = 'red';
+    // this.elementRef.nativeElement.style.backgroundColor = 'red';
+  }
+
+  @HostListener('mouseenter') mouseover(event: Event) {
+    this.renderer.setStyle(this.elementRef.nativeElement, 'border', '1px solid green');
+    // another way
+    this.background = 'blue'
+  }
+
+  @HostListener('mouseleave') mouseleave(event: Event) {
+    this.renderer.setStyle(this.elementRef.nativeElement, 'border', '1px solid red');
+    // another way
+    this.background = 'white'
   }
 
 }
