b3fb07972becb13caaa6ae463fc3003dd811e2a4
Witek85 <witold.szklarski@gmail.com>
2020-08-02 21:17:52 +0200

    login

--- src/app/layout/components/layout/layout.component.ts
+++ src/app/layout/components/layout/layout.component.ts
@@ -1,17 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
 import { MatSnackBar } from '@angular/material';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.css']
 })
-export class LayoutComponent implements OnInit {
+export class LayoutComponent implements OnInit, OnDestroy{
+  isAuthenticated = false;
+  private userSubscription: Subscription;
 
   constructor(private authService:AuthService, private _snackBar: MatSnackBar) { }
 
   ngOnInit() {
+    console.log(this.authService.user);
+    this.userSubscription = this.authService.user.subscribe(user => {
+      this.isAuthenticated = !!user;
+    });
+  }
+
+  ngOnDestroy() {
+    this.userSubscription.unsubscribe();
   }
 
   onLogout() {
