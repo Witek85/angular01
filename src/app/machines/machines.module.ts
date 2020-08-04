ec69ffb8877a3c2b9b17fa3db9ae72fa0252998d
Witek85 <witold.szklarski@gmail.com>
2020-08-04 15:09:39 +0200

    machines list

--- src/app/machines/machines.module.ts
+++ src/app/machines/machines.module.ts
@@ -7,6 +7,7 @@ import { MachinesListComponent } from './components/machines-list/machines-list.
 import { MachinesDetailComponent } from './components/machines-detail/machines-detail.component';
 import { MachinesMapComponent } from './components/machines-map/machines-map.component';
 import { MachineResolver } from './resolvers/machine.resolver.service';
+import { AppMaterialModule } from '../app-material.module';
 
 
 
@@ -14,6 +15,7 @@ import { MachineResolver } from './resolvers/machine.resolver.service';
   declarations: [MachinesComponent, MachinesListComponent, MachinesDetailComponent, MachinesMapComponent],
   imports: [
     CommonModule,
+    AppMaterialModule,
     RouterModule.forChild([
       {path: '', component: MachinesComponent, children: [
         {path: '', component: MachinesMapComponent},
