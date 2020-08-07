83c6f2b912a3368d072b7401e50f045b8de7fcc5
Witek85 <witold.szklarski@gmail.com>
2020-08-07 15:46:32 +0200

    static markers

--- src/app/machines/components/machines-map/machines-map.component.ts
+++ src/app/machines/components/machines-map/machines-map.component.ts
@@ -10,17 +10,49 @@ import { Subscription } from 'rxjs';
   styleUrls: ['./machines-map.component.css']
 })
 export class MachinesMapComponent implements OnInit {
+
+  icon = L.icon({
+    iconSize: [ 25, 41 ],
+    iconAnchor: [ 13, 41 ],
+    iconUrl: 'assets/marker-icon.png',
+    shadowUrl: 'assets/marker-shadow.png'
+  })
+
+  marker1 = L.marker([ 52.2303201, 20.9905102 ], {
+    icon: this.icon
+  });
+
+  marker2 = L.marker([ 52.1945202,20.9955103 ], {
+    icon: this.icon
+  });
+
+  marker3 = L.marker([ 52.2355202,20.9505103 ], {
+    icon: this.icon
+  });
+
+  markersLevel: L.LayerGroup = new L.LayerGroup();
+
+  layerGroup: L.LayerGroup[] = [
+    this.markersLevel
+  ]
+
+  mapLayer = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' });
+
   options = {
     layers: [
-      L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' })
+      this.mapLayer
     ],
-    zoom: 5,
-    center: L.latLng(46.879966, -121.726909)
+    zoom: 11,
+    center: L.latLng(52.2303201, 20.9905102)
   };
 
   constructor() { }
 
   ngOnInit() {
+    this.marker1.addTo(this.markersLevel);
+    this.marker2.addTo(this.markersLevel);
+    this.marker3.addTo(this.markersLevel);
+
   }
 
 
