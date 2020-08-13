29d581547a2e9469be6ef916fadef1f7ab53853a
Witek85 <witold.szklarski@gmail.com>
2020-08-13 13:52:30 +0200

    conditional subheader

--- src/app/layout/components/toolbar/toolbar.component.ts
+++ src/app/layout/components/toolbar/toolbar.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
 import { AuthService } from 'src/app/shared/services/auth.service';
 import { MatSnackBar } from '@angular/material';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-toolbar',
@@ -10,10 +12,18 @@ import { MatSnackBar } from '@angular/material';
 export class ToolbarComponent implements OnInit {
   @Input() isAuthenticated;
   @Output() menuToggle:EventEmitter<null> = new EventEmitter<null>();
+  activeRoute:string = "";
+  isSubheaderVisible:boolean = false;
 
-  constructor(private authService:AuthService, private _snackBar: MatSnackBar) { }
+  constructor(private authService:AuthService, private _snackBar: MatSnackBar, private router: Router) { }
 
   ngOnInit() {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd),
+    ).subscribe(event => {
+      this.activeRoute = event['urlAfterRedirects'];
+      this.isSubheaderVisible = this.activeRoute === "/";
+    });
   }
 
   onMenuClick() {
