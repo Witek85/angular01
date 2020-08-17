922cc1a544d599d69194b918578f1dd4136d30f4
Witek85 <witold.szklarski@gmail.com>
2020-08-17 12:19:42 +0200

    highlight directive test

--- src/app/counter/directives/highlight.directive.spec.ts
+++ src/app/counter/directives/highlight.directive.spec.ts
@@ -1,8 +1,56 @@
+import { TestBed, ComponentFixture } from '@angular/core/testing';
 import { HighlightDirective } from './highlight.directive';
+import { Component, Renderer2, DebugElement } from '@angular/core';
+import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
+import { By } from '@angular/platform-browser';
+
+@Component({
+  template: `<div appHighlight></div>`
+})
+class TestAppHighlightComponent {
+}
 
 describe('HighlightDirective', () => {
+
+  let component: TestAppHighlightComponent;
+  let fixture: ComponentFixture<TestAppHighlightComponent>;
+  let element: DebugElement;
+  let renderer: Renderer2;
+
+  beforeAll( ()=> {
+    TestBed.initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting());
+  });
+
+  beforeEach(() => {
+    TestBed
+      .configureTestingModule({
+        declarations: [TestAppHighlightComponent, HighlightDirective],
+        providers: [Renderer2]
+      });
+
+    fixture = TestBed.createComponent(TestAppHighlightComponent);
+    component = fixture.componentInstance;
+    renderer = fixture.componentRef.injector.get(Renderer2);
+    
+  });
+
   it('should create an instance', () => {
-    const directive = new HighlightDirective();
+    element = fixture.debugElement;
+    const directive = new HighlightDirective(element, renderer);
     expect(directive).toBeTruthy();
   });
+
+  it('hovering over div', () => {
+    element = fixture.debugElement.query(By.css('div'));
+    element.triggerEventHandler('mouseenter', null);
+    fixture.detectChanges();
+    expect(element.nativeElement.style.border).toBe('1px solid green');
+    expect(element.nativeElement.style.backgroundColor).toBe('blue');
+
+    element.triggerEventHandler('mouseleave', null);
+    fixture.detectChanges();
+    expect(element.nativeElement.style.border).toBe('1px solid red');
+    expect(element.nativeElement.style.backgroundColor).toBe('white');
+});
+
 });
