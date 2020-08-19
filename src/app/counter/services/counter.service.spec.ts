b7123a9787a79be9ae9a3033b0d9bd504e882d2e
Witek85 <witold.szklarski@gmail.com>
2020-08-19 11:45:17 +0200

    provide service to service

--- src/app/counter/services/counter.service.spec.ts
+++ src/app/counter/services/counter.service.spec.ts
@@ -3,7 +3,9 @@ import { TestBed } from '@angular/core/testing';
 import { CounterService } from './counter.service';
 
 describe('CounterService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    providers: [ CounterService ]
+  }));
 
   it('should be created', () => {
     const service: CounterService = TestBed.get(CounterService);
