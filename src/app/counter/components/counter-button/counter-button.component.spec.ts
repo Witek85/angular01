99035f92e597b26262e9bb6eb438cb3eeaed9a7f
Witek85 <witold.szklarski@gmail.com>
2020-08-19 16:45:52 +0200

    click emit test

--- src/app/counter/components/counter-button/counter-button.component.spec.ts
+++ src/app/counter/components/counter-button/counter-button.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, tick, fakeAsync } from '@angular/core/testing';
 
 import { CounterButtonComponent } from './counter-button.component';
 
@@ -22,4 +22,13 @@ describe('CounterButtonComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should emit incrementSet on the click', fakeAsync( () => {
+    spyOn(component.incrementSet, 'emit');
+    const nativeElement = fixture.nativeElement;
+    const button = nativeElement.querySelector('button');
+    button.click();
+    tick();
+    expect(component.incrementSet.emit).toHaveBeenCalledWith(1);
+  }));
 });
