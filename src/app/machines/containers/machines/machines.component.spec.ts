64621eef83883945523a843f2680ffe5832442ae
Witek85 <witold.szklarski@gmail.com>
2020-08-19 10:51:26 +0200

    router tests

--- src/app/machines/containers/machines/machines.component.spec.ts
+++ src/app/machines/containers/machines/machines.component.spec.ts
@@ -7,15 +7,31 @@ import { MachinesRoutingModule } from '../../machines.routing.module';
 import { MachinesDetailComponent } from '../../components/machines-detail/machines-detail.component';
 import { AppMaterialModule } from 'src/app/app-material.module';
 import { LeafletModule } from '@asymmetrik/ngx-leaflet';
+import { ActivatedRoute, ChildrenOutletContexts } from '@angular/router';
+import { of } from 'rxjs';
+import { HttpClient, HttpHandler } from '@angular/common/http';
 
 describe('MachinesComponent', () => {
   let component: MachinesComponent;
   let fixture: ComponentFixture<MachinesComponent>;
+  const mockActivatedRoute = { 
+    data: of({ 
+      machine: {
+        _id: '_1234',
+        id: 1,
+        name: "name",
+        lat: 10,
+        lng: 10,
+        teamId: 1,
+      } 
+    }) 
+  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ MachinesComponent, MachinesListComponent, MachinesMapComponent, MachinesDetailComponent ],
-      imports: [ MachinesRoutingModule, AppMaterialModule, LeafletModule ]
+      imports: [ MachinesRoutingModule, AppMaterialModule, LeafletModule ],
+      providers: [ HttpClient, HttpHandler, ChildrenOutletContexts, { provide: ActivatedRoute, useValue: mockActivatedRoute } ],
     })
     .compileComponents();
   }));
--- src/app/todo/components/todos/todos.component.spec.ts
+++ src/app/todo/components/todos/todos.component.spec.ts
@@ -7,17 +7,29 @@ import { AppMaterialModule } from 'src/app/app-material.module';
 import { RandomizePipe } from '../../pipes/randomize.pipe';
 import { ReactiveFormsModule } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
-import { TodoModule } from '../../todo.module';
+import { of } from 'rxjs';
+import { HttpClient, HttpHandler } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('TodosComponent', () => {
   let component: TodosComponent;
   let fixture: ComponentFixture<TodosComponent>;
+  const mockActivatedRoute = { 
+    data: of({ 
+      todo: {
+        _id: '_1234',
+        id: 1,
+        task: "task",
+        priority: "low"
+      } 
+    }) 
+  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ TodosComponent, TodoFormComponent, TodoListComponent, RandomizePipe ],
-      imports: [ AppMaterialModule, ReactiveFormsModule ],
-      providers: [ ActivatedRoute ]
+      imports: [ AppMaterialModule, ReactiveFormsModule, BrowserAnimationsModule ],
+      providers: [ HttpClient, HttpHandler, { provide: ActivatedRoute, useValue: mockActivatedRoute } ],
     })
     .compileComponents();
   }));
