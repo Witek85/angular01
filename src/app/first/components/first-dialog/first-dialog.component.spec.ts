590bed90932e1e448da9c7f1181e65128ae91227
Witek85 <witold.szklarski@gmail.com>
2020-08-20 10:24:35 +0200

    dialog close testing

--- src/app/first/components/first-dialog/first-dialog.component.spec.ts
+++ src/app/first/components/first-dialog/first-dialog.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, tick, fakeAsync } from '@angular/core/testing';
 
 import { FirstDialogComponent } from './first-dialog.component';
 import { AppMaterialModule } from 'src/app/app-material.module';
@@ -7,17 +7,21 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 describe('FirstDialogComponent', () => {
   let component: FirstDialogComponent;
   let fixture: ComponentFixture<FirstDialogComponent>;
+  const dialogMock = {
+    close: () => { }
+  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ FirstDialogComponent ],
       imports: [ AppMaterialModule ],
       providers: [
-        { provide: MAT_DIALOG_DATA, useValue: {} },
-        { provide: MatDialogRef, useValue: {} } 
+        { provide: MAT_DIALOG_DATA, useValue: [] },
+        { provide: MatDialogRef, useValue: dialogMock } 
       ]
     })
     .compileComponents();
+    
   }));
 
   beforeEach(() => {
@@ -29,4 +33,13 @@ describe('FirstDialogComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should close dialog on click', fakeAsync(() => {
+    let spy = spyOn(component.dialogRef, 'close').and.callThrough();
+    const nativeElement = fixture.nativeElement;
+    const button = nativeElement.querySelector('button');
+    button.click();
+    tick();
+    expect(spy).toHaveBeenCalled();
+  }));
 });
